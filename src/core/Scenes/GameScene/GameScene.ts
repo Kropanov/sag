ebd3e1d1c68b9699c00b16db5517059ce638ae50
Maksim Kropanov <kropanov2012@gmail.com>
2024-07-17 23:03:07 +0300

    feat: add tiles and cover bottom of game scene

--- src/core/Scenes/GameScene/GameScene.ts
+++ src/core/Scenes/GameScene/GameScene.ts
@@ -3,25 +3,49 @@ import { IScene } from '../../../interfaces';
 import { Manager } from '../../Manager/Manager';
 
 export class GameScene extends Container implements IScene {
+  private backgroundTiles: Sprite[] = [];
   private bunny: Sprite;
   private bunnyVelocity: number;
   private manager: Manager;
 
   constructor() {
     super();
-
     this.manager = Manager.getInstance();
 
     this.bunny = Sprite.from('bunny');
     this.bunny.anchor.set(0.5);
     this.bunny.x = this.manager.getWidth() / 2;
     this.bunny.y = this.manager.getHeight() / 2;
-    this.addChild(this.bunny);
-
     this.bunnyVelocity = 2;
+
+    this.addChild(this.bunny);
+    this.tileBackground();
   }
 
-  resize(_screenWidth: number, _screenHeight: number): void {}
+  private tileBackground(): void {
+    this.backgroundTiles = [];
+
+    const sprite = Sprite.from('ground');
+    sprite.scale.set(7);
+
+    const tilesNeeded = Math.ceil(this.manager.getWidth() / sprite.width);
+    for (let i = 0; i < tilesNeeded; i++) {
+      const tile = Sprite.from('ground');
+      tile.anchor.set(0, 1);
+      tile.scale.set(7);
+      tile.position.set(i * tile.width, this.manager.getHeight());
+      this.addChild(tile);
+      this.backgroundTiles.push(tile);
+    }
+  }
+
+  resize(_screenWidth: number, _screenHeight: number): void {
+    for (const tile of this.backgroundTiles) {
+      this.removeChild(tile);
+    }
+
+    this.tileBackground();
+  }
 
   public update(framesPassed: number): void {
     this.bunny.x += this.bunnyVelocity * framesPassed;
