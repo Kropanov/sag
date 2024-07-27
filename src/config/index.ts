a52b38d37784d535ad1b4690fc4bc375cb0194a5
Maksim Kropanov <kropanov2012@gmail.com>
2024-07-27 12:34:27 +0300

    feat: implement sound volume in settings

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,4 @@
 // Gravitational acceleration (on Earth it's about 9.81 meter per second per second)
 export const g: number = 0.81;
+
+export const INITIAL_SOUND_VOLUME: number = 0.25;
--- src/core/Settings/Settings.ts
+++ src/core/Settings/Settings.ts
@@ -1,18 +1,25 @@
 import { sound } from '@pixi/sound';
+import { INITIAL_SOUND_VOLUME } from '../../config';
 
 export default class Settings {
   private static instance: Settings;
+
   private constructor() {}
 
   public static getInstance() {
     if (!Settings.instance) {
+      sound.volumeAll = INITIAL_SOUND_VOLUME;
       Settings.instance = new Settings();
     }
 
     return Settings.instance;
   }
 
-  setVolume(value: number) {
-    sound.volumeAll = value;
+  public get volume(): number {
+    return sound.volumeAll * 200;
+  }
+
+  setVolume(value: number): void {
+    sound.volumeAll = value / 200;
   }
 }
