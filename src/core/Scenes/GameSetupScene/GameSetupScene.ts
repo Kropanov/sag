24dc434170bb4e8709d71f9e086823cdd31d4dd7
Maksim Kropanov <kropanov2012@gmail.com>
2024-07-31 23:28:04 +0300

    feat: add a new button in GameSetupScene

--- src/core/Scenes/GameSetupScene/GameSetupScene.ts
+++ src/core/Scenes/GameSetupScene/GameSetupScene.ts
@@ -1,8 +1,12 @@
 import { IScene } from '@/interfaces';
+import { sound } from '@pixi/sound';
 import { FancyButton } from '@pixi/ui';
 import { Container, Graphics, Text } from 'pixi.js';
 
 export class GameSetupScene extends Container implements IScene {
+  private nextButton!: FancyButton;
+  private backButton!: FancyButton;
+
   constructor() {
     super();
 
@@ -14,7 +18,12 @@ export class GameSetupScene extends Container implements IScene {
     container2.rect(500, 200, 400, 600).fill({ color: '#00b1dd' });
     this.addChild(container2);
 
-    const button = new FancyButton({
+    this.drawNextButton();
+    this.drawBackButton();
+  }
+
+  drawNextButton() {
+    this.nextButton = new FancyButton({
       text: new Text({
         text: 'NEXT',
         style: {
@@ -41,10 +50,49 @@ export class GameSetupScene extends Container implements IScene {
       },
     });
 
-    button.x = 473;
-    button.y = 850;
+    this.nextButton.x = 535;
+    this.nextButton.y = 850;
 
-    this.addChild(button);
+    this.addChild(this.nextButton);
+
+    this.nextButton.onHover.connect(() => sound.play('menu_item_click3'));
+    this.nextButton.onPress.connect(() => sound.play('menu_item_click1'));
+  }
+
+  drawBackButton() {
+    this.backButton = new FancyButton({
+      text: new Text({
+        text: 'BACK',
+        style: {
+          fontSize: 25,
+          fill: '#00b1dd',
+          align: 'center',
+        },
+      }),
+      animations: {
+        hover: {
+          props: {
+            scale: { x: 1.1, y: 1.1 },
+            y: 0,
+          },
+          duration: 100,
+        },
+        pressed: {
+          props: {
+            scale: { x: 0.9, y: 0.9 },
+            y: 10,
+          },
+          duration: 100,
+        },
+      },
+    });
+
+    this.backButton.x = 415;
+    this.backButton.y = 850;
+
+    this.addChild(this.backButton);
+    this.backButton.onHover.connect(() => sound.play('menu_item_click3'));
+    this.backButton.onPress.connect(() => sound.play('menu_item_click1'));
   }
 
   next() {}
