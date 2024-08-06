603431ea792a5a1a677b326a53b0f9d52a6f5d23
Maksim Kropanov <kropanov2012@gmail.com>
2024-08-06 14:59:04 +0300

    feat: implement login input

--- src/core/Scenes/AuthScene/AuthScene.ts
+++ src/core/Scenes/AuthScene/AuthScene.ts
@@ -1,16 +1,21 @@
 import { GameManager } from '@/core/Manager';
 import { IScene } from '@/interfaces';
+import { Input } from '@pixi/ui';
 import { Container, Graphics } from 'pixi.js';
 
 export class AuthScene extends Container implements IScene {
   private manager: GameManager = GameManager.getInstance();
   private container: Graphics;
 
+  private inputLogin!: Input;
+
   constructor() {
     super();
 
     this.container = new Graphics();
     this.drawContainer();
+
+    this.drawInputLogin();
   }
 
   drawContainer() {
@@ -26,6 +31,37 @@ export class AuthScene extends Container implements IScene {
     this.container.position.set(this.manager.getWidth() / 2, this.manager.getHeight() / 2);
 
     this.addChild(this.container);
+  }
+
+  drawInputLogin() {
+    this.inputLogin = new Input({
+      bg: new Graphics()
+        .rect(0, 0, this.container.width / 1.5, 40)
+        .fill('#282828')
+        .stroke({
+          color: 'black',
+          width: 1,
+          alignment: 0.5,
+        }),
+      maxLength: 60,
+      textStyle: {
+        fill: '#000000',
+        fontSize: 20,
+        fontWeight: 'bold',
+      },
+      align: 'center',
+      value: '',
+      addMask: false,
+    });
+
+    this.inputLogin.x = 100;
+    this.inputLogin.y = 100;
+
+    this.inputLogin.onChange.connect(() => {
+      console.log(this.inputLogin.value);
+    });
+
+    this.container.addChild(this.inputLogin);
   }
 
   update(_framesPassed: number): void {}
