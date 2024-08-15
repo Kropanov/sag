64414b63c418abc4f556d66291bd719246d7de45
Maksim Kropanov <kropanov2012@gmail.com>
2024-08-15 21:10:28 +0300

    feat: create the mouse event for shooting queue

--- src/core/Weapons/Gun/Gun.ts
+++ src/core/Weapons/Gun/Gun.ts
@@ -5,7 +5,7 @@ import { lerp } from '@/utils';
 import { sound } from '@pixi/sound';
 import { Sprite } from 'pixi.js';
 
-// TODO: optimize and do refactoring
+// TODO: split on files
 export class Gun {
   private keyboard = Keyboard.getInstance();
   private manager = GameManager.getInstance();
@@ -14,12 +14,16 @@ export class Gun {
   private queue: any = [];
   private world: any = [];
 
+  private mouseEvent!: MouseEvent;
+
   private player: any;
 
   private dir: any = {
     x: 0,
     y: 0,
   };
+
+  shootingInterval: any;
 
   constructor(player: any) {
     this.player = player;
@@ -28,7 +32,53 @@ export class Gun {
   }
 
   listen() {
-    window.addEventListener('mousedown', this.click.bind(this));
+    window.addEventListener('mousedown', this.startShooting.bind(this));
+    window.addEventListener('mouseup', this.stopShooting.bind(this));
+    window.addEventListener('mousemove', this.handleMouseMoving.bind(this));
+  }
+
+  startShooting(event: MouseEvent) {
+    if (event.button === 0) {
+      this.shoot(event);
+      this.shootingInterval = setInterval(() => {
+        this.shoot(this.mouseEvent);
+      }, 100);
+    }
+  }
+
+  stopShooting(event: MouseEvent) {
+    if (event.button === 0) {
+      if (this.shootingInterval !== null) {
+        clearInterval(this.shootingInterval);
+        this.shootingInterval = null;
+      }
+    }
+  }
+
+  handleMouseMoving(event: MouseEvent) {
+    this.mouseEvent = event;
+  }
+
+  shoot(event: MouseEvent) {
+    if (this.queue.length === 0) return;
+
+    const x0 = this.player.prevX;
+    const y0 = this.player.prevY;
+
+    const x1 = event.clientX;
+    const y1 = event.clientY;
+
+    this.dir = lerp(x0, y0, x1, y1);
+
+    this.queue[0].bullet.x = x0;
+    this.queue[0].bullet.y = y0;
+    this.queue[0].direction = this.dir;
+
+    const element = this.queue.shift();
+    this.hud.setUIAmmo(this.queue.length);
+    const scene = this.manager.getCurrentScene();
+    scene.addChild(element.bullet);
+    this.world.push(element);
   }
 
   recharge() {
@@ -48,26 +98,6 @@ export class Gun {
     }
 
     this.hud.setUIAmmo(this.queue.length);
-  }
-
-  click({ clientX, clientY }: MouseEvent) {
-    const x0 = this.player.prevX;
-    const y0 = this.player.prevY;
-
-    const x1 = clientX;
-    const y1 = clientY;
-
-    this.dir = lerp(x0, y0, x1, y1);
-
-    this.queue[0].bullet.x = x0;
-    this.queue[0].bullet.y = y0;
-    this.queue[0].direction = this.dir;
-
-    const element = this.queue.shift();
-    this.hud.setUIAmmo(this.queue.length);
-    const scene = this.manager.getCurrentScene();
-    scene.addChild(element.bullet);
-    this.world.push(element);
   }
 
   handleStar(delta: number) {
