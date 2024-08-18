f07813beff3b90662a877a1cfd0471ace779a8a5
Maksim Kropanov <kropanov2012@gmail.com>
2024-08-18 21:04:18 +0300

    feat: finish adding ammo types

--- src/config/ammo.ts
+++ src/config/ammo.ts
@@ -1,10 +1,17 @@
 import { AMMO_TYPE } from '@/types/ammo.enum';
 
 export const AMMO_DAMAGE: { [type in AMMO_TYPE]: number } = {
-  [AMMO_TYPE.DEFAULT]: 4,
+  [AMMO_TYPE.DEFAULT]: 2,
   [AMMO_TYPE.ENERGY]: 8,
   [AMMO_TYPE.FIRE]: 5,
   [AMMO_TYPE.ICE]: 4,
+  [AMMO_TYPE.POISON]: 4,
+  [AMMO_TYPE.TREATMENT]: -8,
+  [AMMO_TYPE.WIND]: 9,
+  [AMMO_TYPE.LIGHTNING]: 15,
+  [AMMO_TYPE.VENOM]: 21,
+  [AMMO_TYPE.DIVINE]: 60,
+  [AMMO_TYPE.ARCANE]: 95,
 };
 
 export const AMMO_EFFECT: { [type in AMMO_TYPE]: any } = {
@@ -12,4 +19,11 @@ export const AMMO_EFFECT: { [type in AMMO_TYPE]: any } = {
   [AMMO_TYPE.ENERGY]: undefined,
   [AMMO_TYPE.FIRE]: undefined,
   [AMMO_TYPE.ICE]: undefined,
+  [AMMO_TYPE.POISON]: undefined,
+  [AMMO_TYPE.TREATMENT]: undefined,
+  [AMMO_TYPE.WIND]: undefined,
+  [AMMO_TYPE.LIGHTNING]: undefined,
+  [AMMO_TYPE.VENOM]: undefined,
+  [AMMO_TYPE.DIVINE]: undefined,
+  [AMMO_TYPE.ARCANE]: undefined,
 };
--- src/types/ammo.enum.ts
+++ src/types/ammo.enum.ts
@@ -1,6 +1,13 @@
 export enum AMMO_TYPE {
   DEFAULT = 'ammo_default',
-  ENERGY = 'ammo_energy',
-  FIRE = 'ammo_fire',
-  ICE = 'ammo_ice',
+  ENERGY = 'ammo_energy', // Critical damage
+  FIRE = 'ammo_fire', // Fire %
+  ICE = 'ammo_ice', // Freezing
+  POISON = 'ammo_poison', // Poison %
+  TREATMENT = 'ammo_treatment', // Treatment
+  WIND = 'ammo_wind', // Repelling
+  LIGHTNING = 'ammo_lightning', // Stunning, +Critical damage
+  VENOM = 'ammo_venom', // Slowing down, Toxin, Reduced armor
+  DIVINE = 'ammo_divine', // Blinding, ++Critical damage, +Loot
+  ARCANE = 'ammo_arcane', // Stasis, Punching, +++Critical damage
 }
