8f807dbbf920b44cc3db02e16b366313d3d57cc1
Maksim Kropanov <kropanov2012@gmail.com>
2024-09-05 23:23:14 +0300

    feat: implement Material entity

---
+++ src/core/Entities/Material.ts
@@ -0,0 +1,8 @@
+import { ItemProps } from '@/interfaces';
+import { Item } from './Abstract/Item';
+
+export class Material extends Item {
+  constructor(props: ItemProps) {
+    super(props);
+  }
+}
