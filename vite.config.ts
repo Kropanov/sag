b8b4c8228e50eefb705f67ebbfc82867d64b66c1
Maksim Kropanov <kropanov@gmail.com>
2024-11-24 11:19:58 +0300

    feat: add vite env (#35)

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
+  define: {
+    'process.env': process.env,
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
