e1c2e653126710353f1c2a83841dd3f113de3d40
Maksim Kropanov <kropanov@gmail.com>
2024-12-18 22:23:29 +0300

    refactor: move inventory components to inventory directory

--- src/core/Display/index.ts
+++ src/core/Display/index.ts
@@ -1,18 +1,18 @@
 export * from './HUDComponent.ts';
 
-export * from './Components/InventoryDraft.ts';
+export * from './Components/Inventory/InventoryDraft.ts';
+export * from './Components/Inventory/SharedChest.ts';
+export * from './Components/Inventory/InventoryBag.ts';
+export * from './Components/Inventory/Inventory.ts';
+export * from './Components/Inventory/InventoryHoverInfoBox.ts';
+export * from './Components/Inventory/LootChest.ts';
 
-export * from './Components/SharedChest.ts';
-export * from './Components/InventoryBag.ts';
-export * from './Components/Inventory.ts';
 export * from './Components/AmmoCounter.ts';
 export * from './Components/ItemAmount.ts';
 export * from './Components/SettingsBox.ts';
 export * from './Components/SettingsMenu.ts';
 export * from './Components/SettingsItemDisplay.ts';
 export * from './Components/CurrentItemDisplay.ts';
-export * from './Components/LootChest.ts';
-export * from './Components/InventoryHoverInfoBox.ts';
 export * from './Components/NotificationBox.ts';
 export * from './HUDController.ts';
 export * from './HUDService.ts';
